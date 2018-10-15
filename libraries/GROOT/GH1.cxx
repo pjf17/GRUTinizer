41d6c348ef8fc4c30d5c473df5e2db2fc1935a69
Brandon Elman <brandonelman@gmail.com>
2018-10-15 09:46:58 -0400

    Added functions to GH1 to allow fitting a horizontal shift

--- libraries/GROOT/GH1.cxx
+++ libraries/GROOT/GH1.cxx
@@ -81,6 +81,44 @@ double GH1::FitEval(double *dim,double *par) {
   return scale * GetBinContent(binNum);
 }
 
+double GH1::FitEvalShift(double *dim,double *par) {
+
+  double x = dim[0] - par[1];
+  //printf("par = 0x%08x\n",par); fflush(stdout);
+  double scale = par[0];
+
+  int binNum = GetXaxis()->FindBin(x); //gHist->GetBin() does not respect rebinning.
+
+  int nBins = GetNbinsX();
+  int kevPerBin = GetXaxis()->GetXmax()/nBins;
+  int curBinX = GetBinCenter(binNum);
+  int nextBinX = GetBinCenter(binNum+1);
+  int prevBinX = GetBinCenter(binNum-1);
+
+  if (x > prevBinX && x <= curBinX){
+    double leftDiff = x - prevBinX;
+    double rightDiff = curBinX - x;
+
+    leftDiff = 1.0 - leftDiff/(double)kevPerBin;   //These numbers are now less than 1
+    rightDiff = 1.0 - rightDiff/(double)kevPerBin; //and a measure of how close it is to that bin
+    double binContentLeft = GetBinContent(binNum-1);
+    double binContentRight = GetBinContent(binNum);
+    return scale * (leftDiff*binContentLeft+rightDiff*binContentRight);
+  }
+
+  else if (x > curBinX && x < nextBinX){
+    double leftDiff = x - curBinX;
+    double rightDiff = nextBinX - x;
+
+    leftDiff = 1.0 - leftDiff/(double)kevPerBin;
+    rightDiff = 1.0 - rightDiff/(double)kevPerBin;
+    double binContentLeft = GetBinContent(binNum);
+    double binContentRight = GetBinContent(binNum+1);
+    return scale * (leftDiff*binContentLeft+rightDiff*binContentRight);
+  }
+  //std::cout << "FAILED IN HISTVALUE!" << std::endl;
+  return scale * GetBinContent(binNum);
+}
 
 TF1  *GH1::ConstructTF1() const {
   if(GetDimension()!=1)
@@ -97,6 +135,21 @@ TF1  *GH1::ConstructTF1() const {
  
 }
 
+TF1  *GH1::ConstructTF1Shift() const {
+  if(GetDimension()!=1)
+    return 0;
+
+  ROOT::Math::ParamFunctor *f = new  ROOT::Math::ParamFunctor((GH1*)this,&GH1::FitEvalShift);
+  double low  = GetXaxis()->GetBinLowEdge(1);
+  double high = GetXaxis()->GetBinUpEdge(GetXaxis()->GetNbins());
+  
+  TF1 *tf1 = new TF1(Form("%s_tf1",GetName()),*f,low,high,1,1);
+  tf1->SetParameter(0,1.0);
+  tf1->SetParameter(1,0.0);
+  tf1->SetNpx(GetXaxis()->GetNbins());  
+  return tf1;
+ 
+}
 
 bool GH1::WriteDatFile(const char *outFile){
   if(strlen(outFile)<1) return 0;
