bce87463f6607c4d856f20608c6e2ac0ad7b7359
Peter Farris <peterfarris@outlook.com>
2021-11-22 15:47:44 -0500

    updated pol histos to have crystal-by-crystal spctra

--- histos/e19002_pol_histos.cxx
+++ histos/e19002_pol_histos.cxx
@@ -314,7 +314,8 @@ void MakeHistograms(TRuntimeObjects& obj) {
               double nnEnergy_corrected = nnhit.GetDopplerYta(s800->AdjustedBeta(GValue::Value("BETA")), s800->GetYta(), &track);
             
               if (prompt_timing_gate->IsInside(timeBank29-nnhit.GetTime(), nnEnergy_corrected)){
-                obj.FillHistogram(dirname, Form("gamma_corrected_addback_n%d_prompt",n), 8192,0,8192, nnEnergy_corrected);
+                obj.FillHistogram(dirname, Form("gamma_corrected_n%d_prompt",n), 8192,0,8192, nnEnergy_corrected);
+                obj.FillHistogram(dirname, Form("gamma_corrected_n%d_crystal%d_prompt",n,nnhit.GetCrystalId()), 8192,0,8192, nnEnergy_corrected);
               }
             }
           }
