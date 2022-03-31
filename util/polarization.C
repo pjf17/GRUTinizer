d9f02b11c39049bf34ff8387d59b7673097bf020
Peter Farris <peterfarris@outlook.com>
2022-03-31 15:52:12 -0400

    draw everything at once

--- util/polarization.C
+++ util/polarization.C
@@ -4,21 +4,27 @@
 #include "TFile.h"
 #include "TH1D.h"
 
-void polarization(TFile *f, std::string folder, int bw, bool swapped=false){
-    std::string hpath = folder;
-    if (swapped) hpath += "/gamma_corrected_swapped_addback_prompt";
-    else hpath += "/gamma_corrected_addback_prompt";
-    std::vector<TH1D*> hists;
-    hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"red_pair")));
-    hists.back()->SetNameTitle("red","red");
-    hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"blue_pair")));
-    hists.back()->SetNameTitle("blue","blue");
-    hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"gold_pair")));
-    hists.back()->SetNameTitle("gold","gold");
+void polarization(TFile *f, std::string folder, int bw){
+    std::vector< std::vector<TH1D*>> hists;
+    for (int i=0; i < 2; i++){
+        std::string hpath = folder;
+        if (i == 1) hpath += "/gamma_corrected_swapped_addback_prompt";
+        else hpath += "/gamma_corrected_addback_prompt";
+        std::vector<TH1D*> temp_hists;
+        temp_hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"red_pair")));
+        temp_hists.back()->SetNameTitle("red","red");
+        temp_hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"blue_pair")));
+        temp_hists.back()->SetNameTitle("blue","blue");
+        temp_hists.push_back( (TH1D*) f->Get(Form("%s_%s",hpath.c_str(),"gold_pair")));
+        temp_hists.back()->SetNameTitle("gold","gold");
+        hists.push_back(temp_hists);
+    }
     
-    //rebin to 16 keV bins
-    for (int i=0; i < 3; i++) {
-        hists[i]->Rebin(bw);
+    //rebin to specified bins
+    for (int i=0; i < 2; i++) {
+        for (int j=0; j < 3; j++) {
+            hists[i][j]->Rebin(bw);
+        }
     }
 
     //normalize
@@ -26,44 +32,78 @@ void polarization(TFile *f, std::string folder, int bw, bool swapped=false){
     int nGB = 13;
 
     //add and subtract
-    std::vector<TH1D*> sum_hists, diff_hists, stat_hists;
-    for (int i=0; i < 3; i++){
-        for (int j=i+1; j < 3; j++){
-            double norm = 1.0;
-            if (i == 0){
-                norm = nRed*1.0/nGB;
+    std::vector<std::vector<TH1D*>> sum_hists, diff_hists, stat_hists;
+    for (int i=0; i < 2; i++){
+        std::vector<TH1D*> temp_sum_hists, temp_diff_hists, temp_stat_hists;
+        for (int j=0; j < 3; j++){
+            for (int k=j+1; k < 3; k++){
+                double norm = 1.0;
+                if (j == 0){
+                    norm = nRed*1.0/nGB;
+                }
+                std::string flag = "dflt";
+                if (i == 1) flag = "swap"; 
+                TH1D *hSum = (TH1D*) hists[i][j]->Clone(Form("%s+%s_%s",hists[i][j]->GetName(),hists[i][k]->GetName(),flag.c_str()));
+                TH1D *hDif = (TH1D*) hists[i][j]->Clone(Form("%s-%s_%s",hists[i][j]->GetName(),hists[i][k]->GetName(),flag.c_str()));
+                TH1D *hStat = (TH1D*) hists[i][j]->Clone(Form("%s-%s/(sigma)_%s",hists[i][j]->GetName(),hists[i][k]->GetName(),flag.c_str()));
+                hSum->Add(hists[i][k],norm);
+                hDif->Add(hists[i][k],-1.0*norm);
+
+                //calc what the expected statistical fluctuation of subtracting the
+                //hist would be if it was all random
+                int nBins = hists[i][j]->GetNbinsX();
+                for (int bin=1; bin <= nBins; bin++){
+                    double difOverErr = 0.0;
+                    if (hists[i][j]->GetBinContent(bin) > 0 || hists[i][k]->GetBinContent(bin) > 0) 
+                        difOverErr = hDif->GetBinContent(bin)/(TMath::Sqrt(hists[i][j]->GetBinContent(bin)) + TMath::Sqrt(hists[i][k]->GetBinContent(bin)*norm));
+
+                    hStat->SetBinContent(bin,difOverErr);
+                }
+
+                temp_sum_hists.push_back(hSum);
+                temp_diff_hists.push_back(hDif);
+                temp_stat_hists.push_back(hStat);
             }
-            TH1D *hSum = (TH1D*) hists[i]->Clone(Form("%s+%s",hists[i]->GetName(),hists[j]->GetName()));
-            TH1D *hDif = (TH1D*) hists[i]->Clone(Form("%s-%s",hists[i]->GetName(),hists[j]->GetName()));
-            TH1D *hStat = (TH1D*) hists[i]->Clone(Form("%s-%s/(sigma)",hists[i]->GetName(),hists[j]->GetName()));
-            hSum->Add(hists[j],norm);
-            hDif->Add(hists[j],-1.0*norm);
-
-            //calc what the expected statistical fluctuation of subtracting the
-            //hist would be if it was all random
-            int nBins = hists[i]->GetNbinsX();
-            for (int bin=1; bin <= nBins; bin++){
-                double difOverErr = 0.0;
-                if (hists[i]->GetBinContent(bin) > 0 || hists[j]->GetBinContent(bin) > 0) 
-                    difOverErr = hDif->GetBinContent(bin)/(TMath::Sqrt(hists[i]->GetBinContent(bin)) + TMath::Sqrt(hists[j]->GetBinContent(bin)*norm));
-
-                hStat->SetBinContent(bin,difOverErr);
-            }
-
-            sum_hists.push_back(hSum);
-            diff_hists.push_back(hDif);
-            stat_hists.push_back(hStat);
         }
+        sum_hists.push_back(temp_sum_hists);
+        diff_hists.push_back(temp_diff_hists);
+        stat_hists.push_back(temp_stat_hists);
     }
     std::string fname = Form("pol_%s.root",folder.c_str());
-    if (swapped) fname = Form("pol_swapped_%s.root",folder.c_str());
     TFile *fOut = new TFile(fname.c_str(),"RECREATE");
     fOut->cd();
-    for (int i=0; i < 3; i++){
-        hists[i]->Write();
-        sum_hists[i]->Write();
-        diff_hists[i]->Write();
-        stat_hists[i]->Write();
+    //draw stuff
+    for (int j=0; j < 3; j++){
+        TCanvas *canv = new TCanvas(diff_hists[0][j]->GetName(),diff_hists[0][j]->GetName());
+        canv->Divide(2,3,0.001,0.001);
+        sum_hists[1][j]->GetXaxis()->SetRangeUser(0,800);
+        diff_hists[1][j]->GetXaxis()->SetRangeUser(0,800);
+        stat_hists[1][j]->GetXaxis()->SetRangeUser(0,800);
+        sum_hists[0][j]->GetXaxis()->SetRangeUser(400,3000);
+        diff_hists[0][j]->GetXaxis()->SetRangeUser(400,3000);
+        stat_hists[0][j]->GetXaxis()->SetRangeUser(400,3000);
+        for (int i=0; i < 2; i++){
+            canv->cd(2-i);
+            stat_hists[i][j]->Draw();
+            canv->cd(4-i);
+            diff_hists[i][j]->Draw();
+            canv->cd(6-i);
+            sum_hists[i][j]->Draw();
+        }
+        canv->Write();
     }
+
+    // //write out the files
+    // std::string fname = Form("pol_%s.root",folder.c_str());
+    // TFile *fOut = new TFile(fname.c_str(),"RECREATE");
+    // fOut->cd();
+    // for (int i=0; i < 2; i++){
+    //     for (int j=0; j < 3; j++){
+    //         hists[i][j]->Write();
+    //         sum_hists[i][j]->Write();
+    //         diff_hists[i][j]->Write();
+    //         stat_hists[i][j]->Write();
+    //     }
+    // }
     return;
 }
