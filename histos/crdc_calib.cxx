649453b60d0bdcc5dc3fa85b215710adc8001730
Peter Farris <peterfarris@outlook.com>
2022-09-07 15:14:24 -0400

    added check for s800

--- histos/crdc_calib.cxx
+++ histos/crdc_calib.cxx
@@ -30,24 +30,26 @@ void MakeHistograms(TRuntimeObjects& obj) {
   TList    *list    = &(obj.GetObjects());
   int numobj = list->GetSize();
 
-  double crdc_1_x = s800->GetCrdc(0).GetDispersiveX();
-  double crdc_2_x = s800->GetCrdc(1).GetDispersiveX();
-  
-  double crdc_1_y = s800->GetCrdc(0).GetNonDispersiveY();
-  double crdc_2_y = s800->GetCrdc(1).GetNonDispersiveY();
-  
-  double crdc1_slope = GValue::Value("CRDC1_Y_SLOPE");
-  double crdc2_slope = GValue::Value("CRDC2_Y_SLOPE");
-  double yhigh = 1200;
-  double ylow = 0;
-  if (! (std::isnan(crdc1_slope) || std::isnan(crdc2_slope))){
-    yhigh = 600;
-    ylow = -600;
-  }
-  obj.FillHistogram("ungated", "crdc1 X_Y", 600, -300, 300, crdc_1_x, 1200, ylow, yhigh, crdc_1_y);  
-  obj.FillHistogram("ungated", "crdc2 X_Y", 600, -300, 300, crdc_2_x, 1200, ylow, yhigh, crdc_2_y);
+  if (s800){
+    double crdc_1_x = s800->GetCrdc(0).GetDispersiveX();
+    double crdc_2_x = s800->GetCrdc(1).GetDispersiveX();
+    
+    double crdc_1_y = s800->GetCrdc(0).GetNonDispersiveY();
+    double crdc_2_y = s800->GetCrdc(1).GetNonDispersiveY();
+    
+    double crdc1_slope = GValue::Value("CRDC1_Y_SLOPE");
+    double crdc2_slope = GValue::Value("CRDC2_Y_SLOPE");
+    double yhigh = 1200;
+    double ylow = 0;
+    if (! (std::isnan(crdc1_slope) || std::isnan(crdc2_slope))){
+      yhigh = 600;
+      ylow = -600;
+    }
+    obj.FillHistogram("ungated", "crdc1 X_Y", 600, -300, 300, crdc_1_x, 1200, ylow, yhigh, crdc_1_y);  
+    obj.FillHistogram("ungated", "crdc2 X_Y", 600, -300, 300, crdc_2_x, 1200, ylow, yhigh, crdc_2_y);
 
-  if(numobj!=list->GetSize()){
-    list->Sort();
+    if(numobj!=list->GetSize()){
+      list->Sort();
+    }
   }
 }
