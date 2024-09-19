cbe9a9516b7c7c5cbab0312202b7d65a2b5d7455
Peter Farris <peterfarris@outlook.com>
2024-09-19 16:44:25 -0400

    added option to normalize along either axis

--- util/norm2dhist.C
+++ util/norm2dhist.C
@@ -1,16 +1,29 @@
 #include "TH2D.h"
 
-void norm2dhist(GH2D *h2) {
+void norm2dhist(GH2D *h2, std::string xy = "y") {
     int nbinsY = h2->GetNbinsY();
     int nbinsX = h2->GetNbinsX();
     TH2D *hclone = new TH2D("normalized","normalized",nbinsX,h2->GetXaxis()->GetXmin(),h2->GetXaxis()->GetXmax(),nbinsY,h2->GetYaxis()->GetXmin(),h2->GetYaxis()->GetXmax());
-    for (int by=1; by <= nbinsY; by++){
-        double norm = h2->Integral(1,nbinsX,by,by);
-        // std::cout<<norm<<std::endl;
-        if (norm < 100) continue;
+    
+    if (xy.find("y") != std::string::npos){
+        for (int by=1; by <= nbinsY; by++){
+            double norm = h2->Integral(1,nbinsX,by,by);
+            if (norm < 100) continue;
+            for (int bx=1; bx <= nbinsX; bx++){
+                hclone->SetBinContent(hclone->GetBin(bx,by),h2->GetBinContent(h2->GetBin(bx,by))*1.0/norm);
+                hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
+            }
+        }
+    } else if (xy.find("x") != std::string::npos) { 
         for (int bx=1; bx <= nbinsX; bx++){
-            hclone->SetBinContent(hclone->GetBin(bx,by),h2->GetBinContent(h2->GetBin(bx,by))*1.0/norm);
-            hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
+            double norm = h2->Integral(bx,bx,1,nbinsY);
+            // std::cout<<norm<<std::endl;
+            // std::cout<<norm<<"\n";
+            if (norm < 100) continue;
+            for (int by=1; by <= nbinsY; by++){
+                hclone->SetBinContent(hclone->GetBin(bx,by),h2->GetBinContent(h2->GetBin(bx,by))*1.0/norm);
+                hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
+            }
         }
     }
     GH2D *hgrut = (GH2D*) hclone;
