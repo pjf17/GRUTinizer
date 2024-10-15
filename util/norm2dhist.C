c82da8f5b4abdeb645294aeb6e7664a1337dea55
Peter Farris <peterfarris@outlook.com>
2024-10-15 16:19:37 -0400

    added option to normalize over both x and y simultaneously

--- util/norm2dhist.C
+++ util/norm2dhist.C
@@ -4,8 +4,11 @@ void norm2dhist(GH2D *h2, std::string xy = "y") {
     int nbinsY = h2->GetNbinsY();
     int nbinsX = h2->GetNbinsX();
     TH2D *hclone = new TH2D("normalized","normalized",nbinsX,h2->GetXaxis()->GetXmin(),h2->GetXaxis()->GetXmax(),nbinsY,h2->GetYaxis()->GetXmin(),h2->GetYaxis()->GetXmax());
+
+    bool isX = xy.find("x") != std::string::npos;
+    bool isY = xy.find("y") != std::string::npos;
     
-    if (xy.find("y") != std::string::npos){
+    if (isY && !isX){
         for (int by=1; by <= nbinsY; by++){
             double norm = h2->Integral(1,nbinsX,by,by);
             if (norm < 100) continue;
@@ -14,15 +17,33 @@ void norm2dhist(GH2D *h2, std::string xy = "y") {
                 hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
             }
         }
-    } else if (xy.find("x") != std::string::npos) { 
+    } 
+    else if (!isY && isX) { 
         for (int bx=1; bx <= nbinsX; bx++){
             double norm = h2->Integral(bx,bx,1,nbinsY);
-            // std::cout<<norm<<std::endl;
-            // std::cout<<norm<<"\n";
             if (norm < 100) continue;
             for (int by=1; by <= nbinsY; by++){
                 hclone->SetBinContent(hclone->GetBin(bx,by),h2->GetBinContent(h2->GetBin(bx,by))*1.0/norm);
                 hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
+            }
+        }
+    }
+    else if (isY && isX) {
+        for (int bx=1; bx <= nbinsX; bx++){
+            double norm = h2->Integral(bx,bx,1,nbinsY);
+            if (norm < 100) continue;
+            for (int by=1; by <= nbinsY; by++){
+                hclone->SetBinContent(hclone->GetBin(bx,by),h2->GetBinContent(h2->GetBin(bx,by))*1.0/norm);
+                hclone->SetBinError(hclone->GetBin(bx,by),h2->GetBinError(h2->GetBin(bx,by))*1.0/norm);
+            }
+        }
+
+        for (int by=1; by <= nbinsY; by++){
+            double norm = h2->Integral(1,nbinsX,by,by);
+            if (norm < 100) continue;
+            for (int bx=1; bx <= nbinsX; bx++){
+                hclone->SetBinContent(hclone->GetBin(bx,by),hclone->GetBinContent(hclone->GetBin(bx,by))*1.0/norm);
+                hclone->SetBinError(hclone->GetBin(bx,by),hclone->GetBinError(hclone->GetBin(bx,by))*1.0/norm);
             }
         }
     }
